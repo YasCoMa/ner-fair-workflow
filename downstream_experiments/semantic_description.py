61281c57df14506dd6fa6293248aacad452a018b
yasmmin <ycfrenchgirl2@gmail.com>
2025-08-21 23:11:45 +0200

    teste

--- downstream_experiments/semantic_description.py
+++ downstream_experiments/semantic_description.py
@@ -6,159 +6,204 @@ from rdflib import Graph, Namespace, URIRef, Literal, RDF, XSD
 from rdflib.namespace import DCTERMS, FOAF, PROV, RDFS, XSD, OWL
 
 class SemanticDescription:
-	
-	def __init__(self, fout):
-		config = json.load( open('/aloy/home/ymartins/match_clinical_trial/experiments/config_biobert.json','r') )
-		self.exp_metadata = {  }
-		if( 'experiment_metadata' in config ):
-			self.exp_metadata = config['experiment_metadata']
-		
-		_id = self.gen_id()
-		self.exp_metadata['id'] = f'exp_{_id}'
-		if( 'name' not in self.exp_metadata):
-			self.exp_metadata['name'] = f'NLP Experiment for namd entity recognition - identifier {_id}'
-		if( 'description' not in self.exp_metadata):
-			self.exp_metadata['description'] = ''
-	
-	def gen_id(self):
-		_id = uuid4().hex
-		return _id
+    
+    def __init__(self, fout):
+        config = json.load( open('/aloy/home/ymartins/match_clinical_trial/experiments/config_biobert.json','r') )
+        self.exp_metadata = {  }
+        if( 'experiment_metadata' in config ):
+            self.exp_metadata = config['experiment_metadata']
+        
+        _id = self.gen_id()
+        self.exp_metadata['id'] = f'exp_{_id}'
+        if( 'name' not in self.exp_metadata):
+            self.exp_metadata['name'] = f'NLP Experiment for namd entity recognition - identifier {_id}'
+        if( 'description' not in self.exp_metadata):
+            self.exp_metadata['description'] = ''
+    
+    def gen_id(self):
+        _id = uuid4().hex
+        return _id
 
-	def _setup_namespaces(self):
-		g = self.graph
-		self.nerwf = Namespace("http://example.org/")
-		g.bind("nerwf", self.nerwf)
-		self.xmlpo = Namespace("https://w3id.org/ontouml-models/model/xhani2023xmlpo/") # https://github.com/OntoUML/ontouml-models/raw/refs/heads/master/models/xhani2023xmlpo/ontology.ttl
-		g.bind("xmlpo", self.xmlpo)
-		self.stato = Namespace("http://purl.obolibrary.org/obo/STATO_")
-		g.bind("stato", self.stato)
-		self.ncit = Namespace("http://purl.obolibrary.org/obo/NCIT_")
-		g.bind("ncit", self.ncit)
-		self.mesh = Namespace("http://id.nlm.nih.gov/mesh/")
-		g.bind("mesh", self.mesh)
-		self.slso = Namespace("http://purl.obolibrary.org/obo/SLSO_")
-		g.bind("slso", self.slso)
-		self.vcard = Namespace("http://www.w3.org/2006/vcard/ns#")
-		g.bind("vcard", self.vcard)
+    def _setup_namespaces(self):
+        g = self.graph
+        self.nerwf = Namespace("http://example.org/")
+        g.bind("nerwf", self.nerwf)
+        self.xmlpo = Namespace("https://w3id.org/ontouml-models/model/xhani2023xmlpo/") # https://github.com/OntoUML/ontouml-models/raw/refs/heads/master/models/xhani2023xmlpo/ontology.ttl
+        g.bind("xmlpo", self.xmlpo)
+        self.stato = Namespace("http://purl.obolibrary.org/obo/STATO_")
+        g.bind("stato", self.stato)
+        self.ncit = Namespace("http://purl.obolibrary.org/obo/NCIT_")
+        g.bind("ncit", self.ncit)
+        self.mesh = Namespace("http://id.nlm.nih.gov/mesh/")
+        g.bind("mesh", self.mesh)
+        self.slso = Namespace("http://purl.obolibrary.org/obo/SLSO_")
+        g.bind("slso", self.slso)
+        self.vcard = Namespace("http://www.w3.org/2006/vcard/ns#")
+        g.bind("vcard", self.vcard)
 
-		self.graph = g
-		
-	def _define_new_onto_elements(self):
-		g = self.graph
-		
-		# Defining specific types of Experiment
-		g.add( ( self.nerwf.MLExperiment, RDFS.subClassOf, self.xmlpo.Experiment ) )
-		g.add(( self.nerwf.MLExperiment, RDFS.label,   Literal("MLExperiment", lang="en")))
-		g.add( ( self.nerwf.NLPExperiment, RDFS.subClassOf, self.nerwf.MLExperiment ) )
-		g.add(( self.nerwf.NLPExperiment, RDFS.label,   Literal("NLPExperiment", lang="en")))
+        self.graph = g
+        
+    def _define_new_onto_elements(self):
+        g = self.graph
+        
+        # Defining specific types of Experiment
+        g.add( ( self.nerwf.MLExperiment, RDFS.subClassOf, self.xmlpo.Experiment ) )
+        g.add(( self.nerwf.MLExperiment, RDFS.label,   Literal("MLExperiment", lang="en")))
+        g.add( ( self.nerwf.NLPExperiment, RDFS.subClassOf, self.nerwf.MLExperiment ) )
+        g.add(( self.nerwf.NLPExperiment, RDFS.label,   Literal("NLPExperiment", lang="en")))
+        g.add( ( self.nerwf.NEREvaluationMeasure, RDFS.subClassOf, self.xmlpo.ClassPredictionEvaluationMeasure ) )
+        g.add(( self.nerwf.NEREvaluationMeasure, RDFS.label,   Literal("NEREvaluationMeasure", lang="en")))
+        
+        # Defining Predictive Learning Models LLM model as a sub class of the general Model class
+        g.add( ( self.mesh.D000098412, RDFS.subClassOf, self.ncit.C43383 ) ) 
+        # Defining LLM model as a sub class of the Predictive Learning Models class
+        g.add( ( self.mesh.D000098342, RDFS.subClassOf, self.mesh.D000098342 ) ) 
+        
+        # --------- Object Properties
+        g.add(( self.nerwf.hasDataset, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.hasDataset, RDFS.domain, self.xmlpo.Experiment ))
+        g.add(( self.nerwf.hasDataset, RDFS.range,  self.xmlpo.Dataset))
+        g.add(( self.nerwf.hasDataset, RDFS.label,   Literal("hasDataset", lang="en")))
+        g.add(( self.nerwf.hasDataset, RDFS.comment,   Literal("Datasets associated to an experiment", lang="en")))
 
-		# Defining Predictive Learning Models LLM model as a sub class of the general Model class
-		g.add( ( self.mesh.D000098412, RDFS.subClassOf, self.ncit.C43383 ) ) 
-		# Defining LLM model as a sub class of the Predictive Learning Models class
-		g.add( ( self.mesh.D000098342, RDFS.subClassOf, self.mesh.D000098342 ) ) 
-		
-		g.add(( self.nerwf.hasDataset, RDF.type, OWL.ObjectProperty) )
-		g.add(( self.nerwf.hasDataset, RDFS.domain, self.xmlpo.Experiment ))
-		g.add(( self.nerwf.hasDataset, RDFS.range,  self.xmlpo.Dataset))
-		g.add(( self.nerwf.hasDataset, RDFS.label,   Literal("hasDataset", lang="en")))
-		g.add(( self.nerwf.hasDataset, RDFS.comment,   Literal("Datasets associated to an experiment", lang="en")))
+        g.add(( self.nerwf.executedBy, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.executedBy, RDFS.label,   Literal("executedBy", lang="en")))
+        g.add(( self.nerwf.executedBy, RDFS.comment,   Literal("Experiments executed by workflow, a step of workflow can also be executed by an algorithm", lang="en")))
 
-		g.add(( self.nerwf.executedBy, RDF.type, OWL.ObjectProperty) )
-		g.add(( self.nerwf.executedBy, RDFS.label,   Literal("executedBy", lang="en")))
-		g.add(( self.nerwf.executedBy, RDFS.comment,   Literal("Experiments executed by workflow, a step of workflow can also be executed by an algorithm", lang="en")))
+        g.add(( self.nerwf.describedBy, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.describedBy, RDFS.domain, self.OWL.Class ))
+        g.add(( self.nerwf.describedBy, RDFS.range,  self.xmlpo.Quality))
+        g.add(( self.nerwf.describedBy, RDFS.label,   Literal("describedBy", lang="en")))
+        g.add(( self.nerwf.describedBy, RDFS.comment,   Literal("Classes may be described by an object of Quality class of its subclasses that specify characteristics", lang="en")))
+        
+        g.add(( self.nerwf.finetunedFrom, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.finetunedFrom, RDFS.domain, self.mesh.D000098342 )) # LLM
+        g.add(( self.nerwf.finetunedFrom, RDFS.range,  self.mesh.D000098342))
+        g.add(( self.nerwf.finetunedFrom, RDFS.label,   Literal("finetunedFrom", lang="en")))
+        g.add(( self.nerwf.finetunedFrom, RDFS.comment,   Literal("Specify whether a new produced ML model was refined from an LLM model", lang="en")))
+        
+        g.add(( self.nerwf.hasParameter, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.hasParameter, RDFS.domain, self.ncit.C43383 )) # Model
+        g.add(( self.nerwf.hasParameter, RDFS.range,  self.xmlpo.parameterSettings))
+        g.add(( self.nerwf.hasParameter, RDFS.label,   Literal("hasParameter", lang="en")))
+        g.add(( self.nerwf.hasParameter, RDFS.comment,   Literal("It can be used to specify the model parameters (hyper parameters)", lang="en")))
+        
+        g.add(( self.nerwf.fromEvaluationMetric, RDF.type, OWL.ObjectProperty) )
+        g.add(( self.nerwf.fromEvaluationMetric, RDFS.domain, self.nerwf.NEREvaluationMeasure )) # Model
+        g.add(( self.nerwf.fromEvaluationMetric, RDFS.range,  self.stato.0000039)) # stato statistic
+        g.add(( self.nerwf.fromEvaluationMetric, RDFS.label,   Literal("fromEvaluationMetric", lang="en")))
+        g.add(( self.nerwf.fromEvaluationMetric, RDFS.comment,   Literal("It can be used to specify the model parameters (hyper parameters)", lang="en")))
+        
+        # --------- Datatype Properties
+        g.add(( self.nerwf.hasReplicateNumber, RDF.type, OWL.DatatypeProperty) )
+        g.add(( self.nerwf.hasReplicateNumber, RDFS.domain, self.OWL.Class )) 
+        g.add(( self.nerwf.hasReplicateNumber, RDFS.range,  XSD.integer))
+        g.add(( self.nerwf.hasReplicateNumber, RDFS.label,   Literal("hasReplicateNumber", lang="en")))
+        g.add(( self.nerwf.hasReplicateNumber, RDFS.comment,   Literal("The replicate index of the asset (model or other output)", lang="en")))
+        
+        g.add(( self.nerwf.belongsToEntity, RDF.type, OWL.DatatypeProperty) )
+        g.add(( self.nerwf.belongsToEntity, RDFS.domain, self.nerwf.NEREvaluationMeasure )) 
+        g.add(( self.nerwf.belongsToEntity, RDFS.range,  XSD.string))
+        g.add(( self.nerwf.belongsToEntity, RDFS.label,   Literal("belongsToEntity", lang="en")))
+        g.add(( self.nerwf.belongsToEntity, RDFS.comment,   Literal("Specify the name of the entity related to the score instance", lang="en")))
+        
+        g.add(( self.nerwf.isAggregatedValue, RDF.type, OWL.DatatypeProperty) )
+        g.add(( self.nerwf.isAggregatedValue, RDFS.domain, self.nerwf.NEREvaluationMeasure )) 
+        g.add(( self.nerwf.isAggregatedValue, RDFS.range,  XSD.boolean))
+        g.add(( self.nerwf.isAggregatedValue, RDFS.label,   Literal("isAggregatedValue", lang="en")))
+        g.add(( self.nerwf.isAggregatedValue, RDFS.comment,   Literal("Describes whether the value of the score instance is a result of an statistical aggregation function (min, max, mean, etc)", lang="en")))
+        
+        self.graph = g
 
-		g.add(( self.nerwf.describedBy, RDF.type, OWL.ObjectProperty) )
-		g.add(( self.nerwf.describedBy, RDFS.domain, self.OWL.Class ))
-		g.add(( self.nerwf.describedBy, RDFS.range,  self.xmlpo.Quality))
-		g.add(( self.nerwf.describedBy, RDFS.label,   Literal("describedBy", lang="en")))
-		g.add(( self.nerwf.describedBy, RDFS.comment,   Literal("Classes may be described by an object of Quality class of its subclasses that specify characteristics", lang="en")))
-		
-		g.add(( self.nerwf.finetunedFrom, RDF.type, OWL.ObjectProperty) )
-		g.add(( self.nerwf.finetunedFrom, RDFS.domain, self.mesh.D000098342 )) # LLM
-		g.add(( self.nerwf.finetunedFrom, RDFS.range,  self.mesh.D000098342))
-		g.add(( self.nerwf.finetunedFrom, RDFS.label,   Literal("finetunedFrom", lang="en")))
-		g.add(( self.nerwf.finetunedFrom, RDFS.comment,   Literal("Specify whether a new produced ML model was refined from an LLM model", lang="en")))
-		
-		g.add(( self.nerwf.hasParameter, RDF.type, OWL.ObjectProperty) )
-		g.add(( self.nerwf.hasParameter, RDFS.domain, self.ncit.C43383 )) # Model
-		g.add(( self.nerwf.hasParameter, RDFS.range,  self.xmlpo.parameterSettings))
-		g.add(( self.nerwf.hasParameter, RDFS.label,   Literal("hasParameter", lang="en")))
-		g.add(( self.nerwf.hasParameter, RDFS.comment,   Literal("It can be used to specify the model parameters (hyper parameters)", lang="en")))
-		
-		g.add(( self.nerwf.hasReplicateNumber, RDF.type, OWL.DatatypeProperty) )
-		g.add(( self.nerwf.hasReplicateNumber, RDFS.domain, self.OWL.Class )) 
-		g.add(( self.nerwf.hasReplicateNumber, RDFS.range,  XSD.integer))
-		g.add(( self.nerwf.hasReplicateNumber, RDFS.label,   Literal("hasParameter", lang="en")))
-		g.add(( self.nerwf.hasReplicateNumber, RDFS.comment,   Literal("It can be used to specify the model parameters (hyper parameters)", lang="en")))
-		
-		self.graph = g
+    def _describe_workflow(self);
+        g = self.graph
+        _id = self.gen_id()
+        _id = f'wf_{_id}'
+        g.add( ( self.nerwf[_id], RDF.type, self.xmlpo.workflow ) )
+        g.add( ( self.nerwf[_id], RDFS.label, Literal( "NER FAIR workflow", lang="en" ) ) )
+        g.add( ( self.nerwf[_id], DCTERMS.subject, Literal( self.exp_metadata['description']) ) )
+        
+        self.graph = g
 
-	def _describe_experiment(self):
-		g = self.graph
-		_id = self.exp_metadata['id']
-		g.add( ( self.nerwf[_id], RDF.type, self.nerwf.NLPExperiment ) )
-		g.add( ( self.nerwf[_id], RDFS.label, Literal( self.exp_metadata['name']) ) )
-		g.add( ( self.nerwf[_id], DCTERMS.description, Literal( self.exp_metadata['description']) ) )
+        return _id
 
-		self.graph = g
+    def _describe_experiment(self):
+        g = self.graph
+        _id = self.exp_metadata['id']
+        g.add( ( self.nerwf[_id], RDF.type, self.nerwf.NLPExperiment ) )
+        g.add( ( self.nerwf[_id], RDFS.label, Literal( self.exp_metadata['name']) ) )
+        g.add( ( self.nerwf[_id], DCTERMS.description, Literal( self.exp_metadata['description']) ) )
+        g.add( ( self.nerwf[_id], self.xmlpo.description, Literal( self.exp_metadata['description']) ) )
+        
+        wfid = self._describe_workflow()
+        g.add( ( self.nerwf[_id], self.nerwf.executedBy, self.nerwf[wfid] ) )
+        
+        self.graph = g
 
-	def _describe_data_preprocessing(self):
-		g = self.graph
-		expid = str(uuid4())
-		g.add( ( self.nerwf[f"experiment/{expid}"], RDF.type, self.xmlpo.Experiment) )
-		
-		self.graph = g
+    def _describe_data_preprocessing(self):
+        g = self.graph
+        expid = str(uuid4())
+        g.add( ( self.nerwf[f"experiment/{expid}"], RDF.type, self.xmlpo.Experiment) )
+        
+        self.graph = g
 
-	def run(self):
-		'''
-		experiment
-			model1 -> hyperparams
+    def run(self):
+        # describe experiment
+        # describe workflow
+        # describe preprocess
+        # describe train
+        # describe preprocess
+        # describe preprocess
 
-		workflow
-			containsProcedure
-				Preprocess (operation)
-					executes PreprocessImplementation
+        '''
+        experiment
+            model1 -> hyperparams
 
-				Train
-					executes SupervisedLearningApproachImplementation
-					SupervisedLearningApproachImplementation implements classificationAlgorithm (mlapproach)
-					NEREvaluationMeasure subclassof ClassPredictionEvaluationMeasure
-					classificationAlgorithm hasOutput DatasetOutTrain
-				
-				MLModelEvaluation executes ModelEvaluationTechnique
-					underContext 'Train'
-					takeDataInput instanceDataset (DatasetOutTrain)
-					generatesScoreDataset score_dataset
-					self:hasScore self:NEREvaluationMeasure (nscore1)
-				
-				nscore1 self:belongsToEntity "intervention"
-				nscore1 fromEvaluationMetric f1, acc, mcc
-				nscore1 isAggregated true
-				nscore1 aggregatedBy max (C), min, mean (ncit:C53319), std (ncit:C53322), median (ncit:c28007)
-				nscore1 hasValue 0.8
+        workflow
+            containsProcedure
+                Preprocess (operation)
+                    executes PreprocessImplementation
 
-				ModelEvaluationTechnique describedBy ModelEvaluationCharacteristic
-				modelEvaluation1 a ModelEvaluationCharacteristic
-				modelEvaluation1 MLEvaluationTechniqueName sklearn_without-prefix
-				modelEvaluation1 MLEvaluationTechniqueDescription sk learn without the prefixes
+                Train
+                    executes SupervisedLearningApproachImplementation
+                    SupervisedLearningApproachImplementation implements classificationAlgorithm (mlapproach)
+                    NEREvaluationMeasure subclassof ClassPredictionEvaluationMeasure
+                    classificationAlgorithm hasOutput DatasetOutTrain
+                
+                MLModelEvaluation executes ModelEvaluationTechnique
+                    underContext 'Train'
+                    takeDataInput instanceTrainSet (DatasetOutTrain)
+                    generatesScoreDataset score_dataset
+                    self:hasScore self:NEREvaluationMeasure (nscore1)
+                
+                nscore1 self:belongsToEntity "intervention"
+                nscore1 fromEvaluationMetric f1, acc, mcc
+                nscore1 isAggregatedValue true
+                nscore1 aggregatedByStatsFunction max (stato:0000151), min(stato:0000150), mean (ncit:C53319), std (ncit:C53322), median (ncit:c28007)
+                nscore1 vcard:hasValue 0.8
 
-				Test
-				Prediction
-					generatesDataset
+                ModelEvaluationTechnique describedBy ModelEvaluationCharacteristic
+                modelEvaluation1 a ModelEvaluationCharacteristic
+                modelEvaluation1 xmlpo:MLEvaluationTechniqueName sklearn_without-prefix
+                modelEvaluation1 xmlpo:MLEvaluationTechniqueDescription sk learn without the prefixes
 
-		preprocess
-			instance dataset (LabeledDataset)
-				rdfs
-		train
+                Test
+                Prediction
+                    generatesDataset
 
-			self:generatesDataset score dataset (Dataset class)
-			self:generatesModel model 1
-			self:generatedBy trainSlaImpl1 (step implementation class)
-			self:hasScore self:NEREvaluationMeasure
-				self:belongsTo self:NEREntity
-				fromEvaluationMetric f1, acc, mcc
-				isAggregated true
-				aggregatedBy max (C), min, mean (ncit:C53319), std (ncit:C53322), median (ncit:c28007)
-				hasValue 0.8
-		test
-		'''
+        preprocess
+            instance dataset (LabeledDataset)
+                rdfs
+        train
+
+            self:generatesDataset score dataset (Dataset class)
+            self:generatesModel model 1
+            self:generatedBy trainSlaImpl1 (step implementation class)
+            self:hasScore self:NEREvaluationMeasure
+                self:belongsTo self:NEREntity
+                fromEvaluationMetric f1, acc, mcc
+                isAggregated true
+                aggregatedBy max (C), min, mean (ncit:C53319), std (ncit:C53322), median (ncit:c28007)
+                hasValue 0.8
+        test
+        '''

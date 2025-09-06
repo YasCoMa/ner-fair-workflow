a87d9cac4f1101c77fd8e8676502330bff969fa7
yasmmin <ycfrenchgirl2@gmail.com>
2025-09-06 14:53:19 +0200

    optune sim metric

--- downstream_experiments/similarity_metrics.py
+++ downstream_experiments/similarity_metrics.py
@@ -1,5 +1,14 @@
 from strsimpy import *
 
+'''
+The following lines must be added in the place of instalation (/aloy/home/ymartins/miniconda3/envs/matchct_env/lib/python3.10/site-packages/strsimpy/) of this package:
+from .jaro_winkler import JaroWinkler
+from .longest_common_subsequence import LongestCommonSubsequence
+from .overlap_coefficient import OverlapCoefficient
+
+So that, it is possible to use the new similarity metrics implemented.
+'''
+
 def compute_levenshtein(a, b):
 	dist = normalized_levenshtein.NormalizedLevenshtein().distance(a, b)
 	return dist

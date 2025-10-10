22b5e53cbc59b1e9f89e06d584e58e4ca03c69ae
yasmmin <ycfrenchgirl2@gmail.com>
2025-10-10 22:07:23 +0200

    teste

--- downstream_experiments/eda.py
+++ downstream_experiments/eda.py
@@ -165,7 +165,8 @@ weighted-avg 0.8282 0.6872(+-0.0031) 0.7273(+-0.0118)"""
 
                         df = pd.read_csv( path, sep= '\t')
                         aux = df[ (df['evaluation_metric'] == m) & (df['stats_agg_name'] == 'mean') ]
-                        dat[mode] = aux.stats_agg_value.max()
+                        if( len(aux) > 0 ):
+                            dat[mode] = aux.stats_agg_value.max()
                     odat = dict( sorted( dat.items(), key=lambda item: item[1], reverse=True ) )
                     best[key][m] = [ list(odat)[0], list(odat.values())[0] ]
                     print(key, m, best[key][m])

e029cc0bc44dc1dbaeb66f9af29d0c5e9611a65f
ymartins <ycfrenchgirl2@gmail.com>
2025-10-15 17:33:45 +0200

    test

--- downstream_experiments/eda_ctpico.py
+++ downstream_experiments/eda_ctpico.py
@@ -8,6 +8,7 @@ import pickle
 import pandas as pd
 from tqdm import tqdm
 import plotly.express as px
+import plotly.graph_objects as go
 from scipy.stats import ranksums
 from scipy import stats
 
@@ -21,10 +22,27 @@ class ExplorationCTPicoResults:
         self.augdir = "/aloy/home/ymartins/match_clinical_trial/experiments/validation/augmented_data/"
         
         self.infolder = '/aloy/home/ymartins/match_clinical_trial/experiments/validation/validation/'
+
+        self.rawinfolder = '/aloy/home/ymartins/match_clinical_trial/experiments/validation/process_ct/clinical_trials/'
+
+        self.tag_file = '/aloy/home/ymartins/match_clinical_trial/experiments/tags.json'
+        self.tags = self.__remove_prefix_tags()
         
         self.out = fout
         if( not os.path.isdir( self.out ) ) :
             os.makedirs( self.out )
+
+    def __remove_prefix_tags(self):
+        target_tags = json.load( open(self.tag_file,'r') )
+        categories = list( map( lambda x: x[2:] if( x[:2].lower() in ['o-', 'b-', 'i-', 'e-', 's-', 'u-', 'l-']) else x, target_tags ) )
+        
+        aux = []
+        for j, c in enumerate(categories):
+            if( c not in aux ):
+                aux.append(c)
+        categories = aux
+
+        return categories
 
     def generate_table_pred_grouped_top(self):
         files = { 'all_predicted': 'predss_results_test_validation.tsv', 'grouped_by_max_ss': 'grouped_predss_results_validation.tsv', 'top_ranked': 'top_grouped_predss_results_validation.tsv' }
@@ -137,16 +155,83 @@ class ExplorationCTPicoResults:
         f.close()
 
         df = pd.read_csv(opath, sep='\t')
+        ents = df[ df['Dataset'] == 'augmented' ].Entity.unique()
         for m in df.Metric.unique():
-            aux = df[ df['Metric'] == m ]
+            aux = df[ (df['Metric'] == m) & (df.Entity.isin(ents) ) ]
             fig = px.bar( aux, x="Entity", y="Count (log10)", text="Count", color='Dataset', title = f"{m} enrichment", barmode='group')
             opath = os.path.join( self.out, f'count_{m}_augds_gold.png')
+            fig.update_layout( width=1200)
             fig.write_image( opath )
+
+    def _get_cts_with_result(self, ids):
+        d = {}
+        opath = os.path.join(self.out, 'cts_with_results.json')
+        if( not os.path.isfile(opath) ):
+            for f in tqdm( os.listdir(self.rawinfolder) ):
+                if( f.startswith('raw') ):
+                    path = os.path.join( self.rawinfolder, f )
+                    dt = json.load( open( path, 'r' ) )
+                    for s in dt:
+                        _id = s["protocolSection"]["identificationModule"]["nctId"]
+                        if( _id in ids ):
+                            d[_id] = 0
+                            if( s['hasResults'] ):
+                                d[_id] = 1
+            json.dump( d, open(opath, 'w') )
+        else:
+            d = json.load( open(opath, 'r') )
+
+        cnt = sum(d.values())
+        return cnt
+
+    def get_statistics_ct_files(self):
+        d = {}
+        cnt = { k: 0 for k in self.tags }
+        procs = os.path.join(self.infolder, 'processed_cts')
+        for f in tqdm( os.listdir( procs ) ):
+            ctid = f.split('.')[0].split('_')[-1]
+            d[ctid] = {}
+            path = os.path.join(procs, f)
+            dt = json.load( open(path, 'r') )
+            for t in self.tags:
+                d[ctid][t] = 0
+                if( t in dt ):
+                    d[ctid][t] = len(dt[t])
+                    cnt[t] += len(dt[t])
+
+        lines = []
+        header = ["CT"] + self.tags + ['total']
+        lines.append( '\t'.join(header) )
+        body = []
+        for k in d:
+            cnts = [ str(x) for x in d[k].values() ]
+            total = str( sum( d[k].values() ) )
+            body.append( [k] + cnts + [total] )
+
+        body = list( map( lambda x: '\t'.join(x), body ))
+        lines += body
+        opath = os.path.join( self.out, 'table_count_tags_in processed_cts.tsv')
+        f = open( opath, 'w')
+        f.write( '\n'.join(lines)+'\n' )
+        f.close()
+        
+        x = list( filter( lambda el: cnt[el] != 0, cnt.keys() ))
+        txt = [ cnt[el] for el in x ]
+        y = [ math.log10(el) for el in txt ]
+        fig = go.Figure( [ go.Bar( x = x, y = y, text = txt) ] )
+        opath = os.path.join( self.out, f'count_tags_in_processed_cts.png')
+        fig.update_layout( xaxis_title = 'Entities', yaxis_title = 'Count (log10)' )
+        fig.write_image( opath )
+
+        ctids = set(d)
+        cnt = self._get_cts_with_result(ctids)
+        print('Total with results: ', cnt)
 
     def run(self):
         #self.generate_table_pred_grouped_top()
         #self.generate_distribution_ssvalue()
         self.comparison_measure_augmentation()
+        #self.get_statistics_ct_files()
 
 if( __name__ == "__main__" ):
     odir = '/aloy/home/ymartins/match_clinical_trial/out_eda_pico'
